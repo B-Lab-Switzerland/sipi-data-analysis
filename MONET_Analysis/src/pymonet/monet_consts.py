167dd96d8c1633b36b1e0213c1015ee82605c8fb
Mischa Knabenhans <mischak@physik.uzh.ch>
2025-07-08 12:36:31 +0200

    Added paths for result files

--- MONET_Analysis/src/pymonet/monet_consts.py
+++ MONET_Analysis/src/pymonet/monet_consts.py
@@ -12,6 +12,12 @@ log_file_processed_s1_data = Path("../data/metadata/processed_s1_data_log.csv")
 log_file_processed_s2_data = Path("../data/metadata/processed_s2_data_log.csv")
 raw_data_dir = Path("../data/raw/")
 processed_data_dir = Path("../data/processed/")
+
+non_redundant_obs_file = Path("../results/non_redundant_observables.csv")
+all_corrmat_file = Path("../results/corrmat_all.pdf")
+pruned_corrmat_file = Path("../results/corrmat_pruned.pdf")
+pruned_metrics_file = Path("../results/pruned_metrics.xlsx")
+
 compact_metrics_filename = "monet2030_metrics.csv" 
 compact_cis_filename = "monet2030_confintervals.csv"
 
