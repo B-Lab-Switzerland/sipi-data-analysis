8fcc72ee2c7b76e181a806821a9877735c5add58
Mischa Knabenhans <mischak@physik.uzh.ch>
2025-07-22 15:43:14 +0200

    Added new file paths and better structure

--- MONET_Analysis/src/pymonet/monet_consts.py
+++ MONET_Analysis/src/pymonet/monet_consts.py
@@ -5,18 +5,38 @@ import pytz
 zurich_tz = pytz.timezone("Europe/Zurich")
 
 # File paths
-indicator_table_path = Path("../data/metadata/indicator_table.csv")
-metainfo_table_path = Path("../data/metadata/monet_datafile_summary_table.csv")
-log_file_raw_data = Path("../data/metadata/raw_data_log.csv")
-log_file_processed_s1_data = Path("../data/metadata/processed_s1_data_log.csv")
-log_file_processed_s2_data = Path("../data/metadata/processed_s2_data_log.csv")
-raw_data_dir = Path("../data/raw/")
-processed_data_dir = Path("../data/processed/")
+# ==========
+# Directories
+results_dir = Path("../results")
+meta_dir = Path("../data/metadata")
+raw_dir = Path("../data/raw")
+processed_dir = Path("../data/processed")
+log_dir = Path("../data/logs")
 
-non_redundant_obs_file = Path("../results/non_redundant_observables.csv")
-all_corrmat_file = Path("../results/corrmat_all.pdf")
-pruned_corrmat_file = Path("../results/corrmat_pruned.pdf")
-pruned_metrics_file = Path("../results/pruned_metrics.xlsx")
+# Meta files
+capmap_path = meta_dir / "capitals_map.csv"
+trend_directions = meta_dir / "trend_directions.csv"
+key_indicators = meta_dir / "key_indicators.csv"
+indicator_table_path = meta_dir / "indicator_table.csv"
+metainfo_table_path = meta_dir / "monet_datafile_summary_table.csv"
+
+# Log files
+log_file_raw_data = log_dir / "raw_data_log.csv"
+log_file_processed_s1_data = log_dir / "processed_s1_data_log.csv"
+log_file_processed_s2_data = log_dir / "processed_s2_data_log.csv"
+
+# Result files
+non_redundant_obs_file = results_dir / "non_redundant_observables.csv"
+pruned_metrics_file = results_dir / "pruned_metrics.xlsx"
+top3_metrics_file = results_dir / "top3_metrics.xlsx"
+bottom3_metrics_file = results_dir / "bottom3_metrics.xlsx"
+top3_metrics_per_cap_file = results_dir / "top3_metrics_per_capital.xlsx"
+bottom3_metrics_per_cap_file = results_dir / "bottom3_metrics_per_capital.xlsx"
+key_indicator_performance_file = results_dir / "key_indicator_performance_ranking.xlsx"
+
+all_corrmat_file = results_dir / "corrmat_all.pdf"
+pruned_corrmat_file = results_dir / "corrmat_pruned.pdf"
+n_key_indicators_per_performance_plot = results_dir / "n_key_indicators_per_performance_group.png"
 
 compact_metrics_filename = "monet2030_metrics.csv" 
 compact_cis_filename = "monet2030_confintervals.csv"
